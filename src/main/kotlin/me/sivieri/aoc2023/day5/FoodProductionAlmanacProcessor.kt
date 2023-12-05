60ea8fd590ac1895ebd24080df038f3d9629a27e
Alessandro Sivieri <alessandro.sivieri@gmail.com>
2023-12-05 19:25:49 +0100

    Day 5 part 2

--- src/main/kotlin/me/sivieri/aoc2023/day5/FoodProductionAlmanacProcessor.kt
+++ src/main/kotlin/me/sivieri/aoc2023/day5/FoodProductionAlmanacProcessor.kt
@@ -21,15 +21,17 @@ class FoodProductionAlmanacProcessor(data: String) {
 
     fun findLowestLocationNumberV2(): Long = almanacV2
         .seedRanges
-        .flatMap { it.start..it.end }
-        .minOfOrNull { seed ->
-            val soil = findRange(seed, almanac.seedToSoil)
-            val fertilizer = findRange(soil, almanac.soilToFertilizer)
-            val water = findRange(fertilizer, almanac.fertilizerToWater)
-            val light = findRange(water, almanac.waterToLight)
-            val temperature = findRange(light, almanac.lightToTemperature)
-            val humidity = findRange(temperature, almanac.temperatureToHumidity)
-            findRange(humidity, almanac.humidityToLocation)
+        .minOfOrNull { seedRange ->
+            println("Seed range $seedRange")
+            (seedRange.start..seedRange.end).minOfOrNull { seed ->
+                val soil = findRange(seed, almanac.seedToSoil)
+                val fertilizer = findRange(soil, almanac.soilToFertilizer)
+                val water = findRange(fertilizer, almanac.fertilizerToWater)
+                val light = findRange(water, almanac.waterToLight)
+                val temperature = findRange(light, almanac.lightToTemperature)
+                val humidity = findRange(temperature, almanac.temperatureToHumidity)
+                findRange(humidity, almanac.humidityToLocation)
+            }!!
         }!!
 
     private fun findRange(value: Long, ranges: List<AlmanacRangesInclusive>): Long = ranges
